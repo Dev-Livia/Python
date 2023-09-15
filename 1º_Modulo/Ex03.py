75f6eb9767e27708673a725a566e6a4219cc8d8a
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-09-15 05:38:32 -0300

    Create Ex03.py

---
+++ 1º_Modulo/Ex03.py
@@ -0,0 +1,5 @@
+#Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+print(f'A soma entre esses números é igual a {n1 + n2}')
