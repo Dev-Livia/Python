1a3fcd27c73643cccfe1cd43131f898b11cd6aba
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-10-03 17:03:55 -0300

    Create Ex07.py

---
+++ 1º_Modulo/Ex07.py
@@ -0,0 +1,5 @@
+#Exercício Python #007 - Média Aritmética
+n1 = float(input('Informe a primeira nota: '))
+n2 = float(input('Informe a segunda nota: '))
+média = (n1 + n2) / 2
+print(f'A média do aluno é {média}')
