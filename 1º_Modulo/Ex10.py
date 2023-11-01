9bbf7f01528b148d9ded4f7171168a7e709771de
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-11-01 17:01:20 -0300

    Create Ex10.py

---
+++ 1º_Modulo/Ex10.py
@@ -0,0 +1,6 @@
+#Exercício Python 10: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+
+Real = float(input('Informe quanto você deseja converter:R$ '))
+dolar = ( Real / 5 )
+
+print(f'R${Real} você terá {dolar} Dolares ')
