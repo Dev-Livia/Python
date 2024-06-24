99a2e7f61276307eaa5d114750da8ebb8f4998df
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-06-24 15:48:54 -0300

    up

---
+++ Lista_01/Ex02.py
@@ -0,0 +1,8 @@
+#2.	Faça um programa que declare 4 variáveis do tipo int, em que cada uma dessas variáveis deve possuir o mesmo nome, mas alterando o nome entre maiúsculas e minúsculas (ideia é verificar que a linguagem é case sensitive). Depois imprima essas variáveis na tela. 
+
+num = 1
+Num = 2
+NUM = 3
+NuM = 4
+
+print(num,Num,NUM,NuM)
---
+++ Lista_01/Ex03.py
@@ -0,0 +1,6 @@
+#3.	Faça um programa que calcule a média de dois números. 
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+
+print(f'A media do aluno é {media}')
