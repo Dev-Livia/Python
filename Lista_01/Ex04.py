45cfa195e162845d5900315b979281ef2aa5a657
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-06-24 16:04:43 -0300

    up

---
+++ Lista_01/Ex04.py
@@ -0,0 +1,4 @@
+#4.	Faça um programa calcule o resto da divisão e imprima o resultado na tela.  
+num1 = int(input("Digite um numero para ver o resto da sua divisão: "))
+div = num1 % 2
+print(div)
---
+++ Lista_01/Ex06.py
@@ -0,0 +1,1 @@
+#7.	Faça um programa que recebe dois números e indica qual é o maior.  
